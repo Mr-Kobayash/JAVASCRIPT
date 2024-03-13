0a6edb24e290c280835113551a0e81b6d7d562e4
Mr-Kobayash <122468040+Mr-Kobayash@users.noreply.github.com>
2024-03-12 21:50:56 -0300

    novos ex

---
+++ aula11/ex08.js
@@ -0,0 +1,7 @@
+var vel = 60.5;
+
+console.log(`A velocidade do seu carro é ${vel}KM/H`);
+if (vel > 60) { // condição simples! 
+  console.log("Você ultrapassou a velocidade permitida. MULTADO !");
+}
+console.log(`Diriga com cinto de segurança`);
