c7215caca382d2273b21e6be68c6e933818d554d
Mr-Kobayash <122468040+Mr-Kobayash@users.noreply.github.com>
2024-03-13 00:21:05 -0300

    continuar

---
+++ aula11/ex11.js
@@ -0,0 +1,11 @@
+var idade = 67;
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+  console.log("Não Vota");
+} else if (idade < 18 || idade > 65) {
+   console.log("Voto opcional");
+} else if (idade >= 18) {
+    console.log ('Voto Obrigatorio')
+}
+
+// Eu parei no 00h:14m:20s
